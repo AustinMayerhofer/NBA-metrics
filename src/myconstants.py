ef657df7176d1ea3352532ead81fee4280352416
AustinMayerhofer <austinm41.99@gmail.com>
2022-02-20 20:13:35 -0800

    initial ranking algorithm

--- src/myconstants.py
+++ src/myconstants.py
@@ -1,3 +1,4 @@
 EVERYTHING_AFTER_BACKSLASH = '(?<=\\\\).+' # regex for anything after a backslash
 EVERYTHING_AFTER_DASH = '(?<=-).+' # regex for anything after a dash
 ALPHABETICALLY_LAST_STRING = 'ZZZ' # string that comes last alphabetically in a sort
+NUM_GAMES_2020_21 = 72
