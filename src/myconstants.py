60232ad8b2642f2a2ae5dcb76de3600962ef42c2
AustinMayerhofer <austinm41.99@gmail.com>
2022-04-17 20:00:26 -0700

    remove TEST variable in constants file

--- src/myconstants.py
+++ src/myconstants.py
@@ -3,8 +3,6 @@ EVERYTHING_AFTER_DASH = '(?<=-).+' # regex for anything after a dash
 ALPHABETICALLY_LAST_STRING = 'ZZZ' # string that comes last alphabetically in a sort
 NUM_GAMES_2020_21 = 72
 
-TEST = 5
-
 TEAM_NAME = {
     'ATL': 'Atlanta Hawks',
     'BOS': 'Boston Celtics',
